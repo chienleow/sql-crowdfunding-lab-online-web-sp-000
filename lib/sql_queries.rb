0e53d8a814a77e20fe7a951d2768269753ce9c25
C. Leow <chienleow1@gmail.com>
2020-02-23 02:43:06 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -48,7 +48,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   FROM projects
   INNER JOIN pledges
   ON pledges.project_id = projects.id
-  GROUP BY pledges.amount
+  ORDER BY pledges.amount
   HAVING projects.category = 'music';"
 end
 
