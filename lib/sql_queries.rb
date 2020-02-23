87daf7205e6d6b4ff63e0102d01fdcf31da9d4b8
C. Leow <chienleow1@gmail.com>
2020-02-23 02:44:25 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -48,7 +48,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   FROM projects
   INNER JOIN pledges
   ON pledges.project_id = projects.id
-  GROUP BY projects.category HAVING projects.category = 'music';"
+  GROUP BY pledges.amount HAVING projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
